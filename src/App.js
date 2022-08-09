8bc059f316377a1e0cccf8d691f2e3b9c281b76a
Barış Avtan <baris.avtan@arvento.com>
2022-08-09 16:57:57 +0300

    Radio App Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React from 'react';
+import { DATA } from './Data';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 function App() {
+  const [url,setUrll] =useState(null)
+  
+  const changeUrl =(url) =>{
+    setUrll(url);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='myHeader'>My Station</div>
+      <div className="App">
+        {DATA.map((item) => {
+          return (
+            <a onClick={()=>setUrll(item.url)} className="radio-station">
+              <img src={item.image}></img>
+            </a>
+          )
+        })}
+      </div>
+      <div className='player'>
+
+        <AudioPlayer
+          autoPlay
+          preload={'none'}
+          showJumpControls={false}
+          src={url}
+          onPlay={e => console.log("onPlay")}
+        // other props here
+        />
+
+      </div>
     </div>
   );
 }
